96b763cdb81896ee51e3b16b9e07c5fb629d137a
Icey-Python <ndungusamkelly5@gmail.com>
2023-12-20 22:23:24 +0300

    Added: Map locator to API

--- backend/api/api.py
+++ backend/api/api.py
@@ -10,6 +10,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# use OSM for maps
+import folium
+
 app = Flask(__name__)
 CORS(app)
 @app.get('/')
@@ -53,5 +56,52 @@ def send_email():
 
     return {"status": "success"}
 
+#map location
+def generate_map_html(start_lat, start_lon, end_lat, end_lon):
+
+    # Create the map
+    my_map = folium.Map(location=[(start_lat+end_lat)/2, (start_lon+end_lon)/2], zoom_start=5)
+
+    # Add start and end location markers
+    folium.Marker([start_lat, start_lon], popup='Start').add_to(my_map)
+    folium.Marker([end_lat, end_lon], popup='End').add_to(my_map)  
+
+    # Connect markers with polyline
+    line = folium.PolyLine(locations=[[start_lat, start_lon], [end_lat, end_lon]])
+    my_map.add_child(line)
+
+    # Get map HTML
+    map_html = '''
+        <html>
+        <head>
+        <title>My Map</title>
+        </head>
+        <body>'''+my_map.get_root()._repr_html_()+'''
+        </body>
+        </html>
+        '''
+
+    return map_html
+
+@app.post('/locate')
+@cross_origin()
+def locate():
+    """
+    required json data
+    {
+        latitude: str,
+        longitude: str,
+    }
+    """
+    data :dict = request.get_json()
+    latitude = data['latitude']
+    longitude = data['longitude']
+    end_lat = -1.276702
+    end_long = 36.811468
+    #call the OSM mapping function here
+    
+    return jsonify({"message":{
+        "location":generate_map_html(latitude,longitude,end_lat,end_long)
+        }})
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
